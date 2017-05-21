ce91125956befa1c368687cca078bd9316f2e22b
Ilter Cengiz <iltercengiz@yahoo.com>
2017-05-21 16:26:19 +0300

    Added search request model

---
+++ Premier/Networking/Requests/Search/SearchMoviesRequest.swift
@@ -0,0 +1,30 @@
+//
+//  SearchMoviesRequest.swift
+//  Premier
+//
+//  Created by Ilter Cengiz on 19/05/2017.
+//  Copyright © 2017 Deliveroo. All rights reserved.
+//
+
+import Foundation
+
+struct SearchMoviesRequest: Request {
+    
+    var method: HTTPMethod = .get
+    var path: String = "/search/movie"
+    var parameters: [String : Any] {
+        return [
+            "query": query,
+            "page": page
+        ]
+    }
+    
+    private let query: String
+    private let page: Int
+    
+    init(query: String, page: Int) {
+        self.query = query
+        self.page = page
+    }
+    
+}
